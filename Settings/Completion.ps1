04b4eb83ff735268c142b120bc53a4dbc2e68adf
daephx <daephxdev@gmail.com>
2024-07-30 12:52:06 -0700

    feat: add dotnet CLI completion and include PowerShell version check
    - Added check to ensure backward compatibility with PowerShell 5.1.
    - Added parameter completion for `dotnet` CLI commands.
    - Retained existing completion support for `winget` CLI.

--- Settings/Completion.ps1
+++ Settings/Completion.ps1
@@ -1,3 +1,16 @@
+# PowerShell completion shims for CLI commands.
+# Not supported in earlier versions of Windows PowerShell.
+if ($PSVersionTable.PSVersion.Major -lt 6) { return }
+
+# PowerShell parameter completion shim for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+    dotnet complete --position $cursorPosition "$commandAst" | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
+
+# PowerShell parameter completion shim for the winget CLI
 Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
     param($wordToComplete, $commandAst, $cursorPosition)
     [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
