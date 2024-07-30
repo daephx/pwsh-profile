5d7d35036e1be6f69fbb83efe233bb4d5db8b0b8
daephx <daephxdev@gmail.com>
2024-07-30 12:36:58 -0700

    fix: use `Set-ItemProperty` for persistent environment variables
    - Replaced `$ENV:VAR` with `Set-ItemProperty` to set persistent
      environment variables for external Python utilities.
    - Added `PIPX_HOME` variable to control the default installation
      location for `pipx`.
    
    `[environment]::setEnvironmentVariable` was tested but found
    unsuitable due to performance issues.

--- Settings/Python.ps1
+++ Settings/Python.ps1
@@ -1,9 +1,10 @@
 # Python environment settings
 if (-not (Get-Command python -ErrorAction Ignore)) { return }
 
-# Disable environment prompt for conda
-$ENV:CONDA_AUTO_ACTIVATE_BASE = $false
-$ENV:VIRTUAL_ENV_DISABLE_PROMPT = $true
+# Set user environment variables
+Set-ItemProperty -Path HKCU:\Environment -Name 'CONDA_AUTO_ACTIVATE_BASE' -Value $false
+Set-ItemProperty -Path HKCU:\Environment -Name 'VIRTUAL_ENV_DISABLE_PROMPT' -Value $true
+Set-ItemProperty -Path HKCU:\Environment -Name 'PIPX_HOME' -Value "$ENV:LOCALAPPDATA\pipx"
 
 function Global:Invoke-Python { & (Get-Command "python").source @args }
 Set-Alias py Invoke-Python
